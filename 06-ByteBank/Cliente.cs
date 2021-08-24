26731c958e344a5290a59b80c80fdb60d42c5d50
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-08-24 17:48:50 -0300

    Add files via upload

---
+++ 06-ByteBank/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_ByteBank
+{
+    public class Cliente
+    {
+        //digito prop tab+tab e ocorre o auto preenchimento, só edito o que precisa 
+        public string Nome { get; set; }
+        private string _cpf;
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                // Escrevo minha lógica de validação de CPF
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+    }
+}
+
